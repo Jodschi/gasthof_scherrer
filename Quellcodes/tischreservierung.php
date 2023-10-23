587fc859e8daac9afc1f5bea6b912be5ce55121b
LukasRatzesberger <148195058+LukasRatzesberger@users.noreply.github.com>
2023-10-23 08:50:02 +0200

    stand 16.10.23

---
+++ Quellcodes/tischreservierung.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Ihr Name">
+    <title>Tischreservierung</title>
+    <link rel="stylesheet" href="style.css"> <!-- Verknüpfen Sie Ihre CSS-Datei hier -->
+</head>
+<body>
+    <header>
+        <h1>Tischreservierung</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="gasthofscherrer.php">Startseite</a></li>
+            <li><a href="speisekarte.php">Speisekarte</a></li>
+            <li><a href="zimmerbuchung.php">Zimmerbuchung</a></li>
+            <li><a href="tischreservierung.php">Tischreservierung</a></li>
+            <li><a href="kontakt.php">Kontakt</a></li>
+            <li><a href="datenschutz.php">Datenschutz</a></li>
+            <li><a href="impressum.php">Impressum</a></li>
+        </ul>
+    </nav>
+    <form action="reservierung.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required placeholder = "Max Mustermann">
+        
+        <label for="email">E-Mail:</label>
+        <input type="email" id="email" name="email" required placeholder = "email@example.com">
+        
+        <label for="phone">Telefonnummer:</label>
+        <input type="tel" id="phone" name="phone" required placeholder = "0123456789">
+        
+        <label for="date">Datum:</label>
+        <input type="date" id="date" name="date" required placeholder = "01.01.2020">
+        
+        <label for="time">Uhrzeit:</label>
+        <input type="time" id="time" name="time" required placeholder = "00:00">
+        
+        <label for="party-size">Größe der Gruppe:</label>
+        <input type="number" id="party-size" name="party-size" min="1" required placeholder = "0">
+        
+        <label for="special-requests">Besondere Wünsche:</label>
+        <textarea id="special-requests" name="special-requests"></textarea>
+        
+        <label for="newsletter">Newsletter abonnieren:
+        <input type="checkbox" id="newsletter" name="newsletter"></label>
+        <br>
+        <input type="submit" value="Reservierung abschicken">
+    </form>
+
+    <footer>
+        &copy; Gasthof Scherrer
+    </footer>
+</body>
+</html>
---
+++ Quellcodes/zimmerbuchung.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Ratzesberger, Reiter">
+    <title>Zimmerbuchung</title>
+    <link rel="stylesheet" href="style.css"> <!-- Verknüpfen Sie Ihre CSS-Datei hier -->
+</head>
+
+<body>
+    <header>
+        <h1>Zimmerbuchung</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="gasthofscherrer.php">Startseite</a></li>
+            <li><a href="speisekarte.php">Speisekarte</a></li>
+            <li><a href="zimmerbuchung.php">Zimmerbuchung</a></li>
+            <li><a href="tischreservierung.php">Tischreservierung</a></li>
+            <li><a href="kontakt.php">Kontakt</a></li>
+            <li><a href="datenschutz.php">Datenschutz</a></li>
+            <li><a href="impressum.php">Impressum</a></li>
+        </ul>
+    </nav>
+    <form action="zimmerbuchung.php" method="post">
+        <br>
+        <label for="name">Name:</label>
+        <input type="text" id="name" placeholder = "Max Mustermann" name="name" required >
+        <br>
+        <form class="row g-3">
+            <div class="col-auto">
+                <label for="staticEmail2" class="visually-hidden">Email</label>
+                <input type="text" class="form-control-plaintext" id="staticEmail2" placeholder="email@example.com">
+            </div>
+            <div class="col-auto">
+                <label for="inputPassword2" class="visually-hidden">Passwort</label>
+                <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-3">Identität bestätigen</button>
+            </div>
+            <div><label for="check-in">Check-In Datum:</label>
+                <input type="date" id="check-in" name="check-in" required>
+            </div>
+            <div><label for="check-out">Check-Out Datum:</label>
+                <input type="date" id="check-out" name="check-out" required>
+            </div>
+            <div><label for="room-type">Zimmertyp:</label>
+                <select id="room-type" name="room-type">
+                    <option value="Einzelzimmer">Einzelzimmer</option>
+                    <option value="Doppelzimmer">Doppelzimmer</option>
+                    <option value="Suite">Suite</option>
+                    <br>
+                </select>
+            </div>
+            <div> <label for="special-requests">Besondere Wünsche:</label>
+                <textarea id="special-requests" name="special-requests"></textarea>
+            </div>
+            <div><input type="submit" value="Buchung abschicken"></div>
+        </form>
+        <footer>
+            &copy; Gasthof Scherrer
+        </footer>
+</body>
+
+</html>
