1565f0d5293da44c37235a8bc40982ae5adc602c
Jodschi <johannes.reiter@tfs-haslach.at>
2023-11-20 09:46:15 +0100

    ben
    ben

---
+++ Stand2023_10_30/header.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {stickyHeader()};
+
+const navbar = document.querySelector('.sticky-header');
+const header = document.querySelector('header');
+
+let sticky = header.clientHeight;
+
+function stickyHeader() {
+    if (window.scrollY > sticky)  {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
