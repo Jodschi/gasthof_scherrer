782574bf23ec372ba8907601719a79568d0ecd83
Jodschi <johannes.reiter@tfs-haslach.at>
2023-11-23 21:25:05 +0100

    Hamburger Icon links oben hinzugefügt

--- Stand2023_11_20/header.js
+++ Stand2023_11_20/header.js
@@ -3,10 +3,11 @@ window.onscroll = function() {stickyHeader()};
 const navbar = document.querySelector('.sticky-header');
 const header = document.querySelector('header');
 
+
 let sticky = header.clientHeight;
 
 function stickyHeader() {
-    if (window.scrollY > sticky)  {
+    if (window.scrollY > (sticky))  {
         navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
