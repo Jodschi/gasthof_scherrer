4914cf269f92a589a464c827c55e8375c7e6295e
Jodschi <148190301+Jodschi@users.noreply.github.com>
2023-12-11 10:40:03 +0100

    Stand 11.12.2023

--- Stand7/header.js
+++ Stand7/header.js
@@ -1,7 +1,7 @@
 window.onscroll = stickyHeader;
 window.onload = stickyHeader;
 
-const navbar = document.querySelector('.sticky-header');
+const navbar = document.querySelector('#sticky-header');
 const header = document.querySelector('header');
 
 
@@ -10,9 +10,11 @@ let sticky = header.clientHeight;
 function stickyHeader() {
     if (window.scrollY > sticky)  {
         navbar.classList.add("sticky");
+        navbar.classList.add("border");
         sidebar.classList.add("sidebar_sticky");
     } else {
         navbar.classList.remove("sticky");
+        navbar.classList.remove("border");
         sidebar.classList.remove("sidebar_sticky");
     }
 }
