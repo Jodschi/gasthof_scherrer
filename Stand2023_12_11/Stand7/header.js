1369f4d73be9e8a55f815a9746e9c7f7247501f6
Jodschi <johannes.reiter@tfs-haslach.at>
2023-12-17 20:43:35 +0100

    Header-Animation beim scrollen hinzugefüt

--- Stand2023_12_11/Stand7/header.js
+++ Stand2023_12_11/Stand7/header.js
@@ -10,11 +10,11 @@ let sticky = header.clientHeight;
 function stickyHeader() {
     if (window.scrollY > sticky)  {
         navbar.classList.add("sticky");
-        navbar.classList.add("border");
+        navbar.classList.add("bottom_line");
         sidebar.classList.add("sidebar_sticky");
     } else {
         navbar.classList.remove("sticky");
-        navbar.classList.remove("border");
+        navbar.classList.remove("bottom_line");
         sidebar.classList.remove("sidebar_sticky");
     }
 }
