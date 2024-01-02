7e033d942147bc383a5806144b476b830038debe
Jodschi <johannes.reiter@tfs-haslach.at>
2024-01-02 16:49:36 +0100

    sidebar animation erstellt und generell sidebar gestylt

--- Stand2023_12_11/Stand7/header.js
+++ Stand2023_12_11/Stand7/header.js
@@ -4,6 +4,10 @@ window.onload = stickyHeader;
 const navbar = document.querySelector('#sticky-header');
 const header = document.querySelector('header');
 
+if(document.querySelector('sidebar-wrapper') != null){
+    const sidebar_wrapper = document.querySelector('sidebar-wrapper');
+};
+
 
 let sticky = header.clientHeight;
 
@@ -11,11 +15,11 @@ function stickyHeader() {
     if (window.scrollY > sticky)  {
         navbar.classList.add("sticky");
         navbar.classList.add("bottom_line");
-        sidebar.classList.add("sidebar_sticky");
+        sidebar_wrapper.classList.add("sidebar_sticky");
     } else {
         navbar.classList.remove("sticky");
         navbar.classList.remove("bottom_line");
-        sidebar.classList.remove("sidebar_sticky");
+        sidebar_wrapper.classList.remove("sidebar_sticky");
     }
 }
 
--- Stand2023_12_11/Stand7/sidebar.js
+++ Stand2023_12_11/Stand7/sidebar.js
@@ -1,16 +1,38 @@
 const sidebar = document.querySelector('#sidebar');
 const sidebar_wrapper = document.querySelector('.sidebar-wrapper');
+const sidebar_links = document.querySelector('.sidebar-links');
 const hamburger_icon = document.querySelector('.hamburger_button');
 
-// margin Top des sidebar_wrappers wird automatisch angepasst, damit keine Zwischenräume entstehen
-sidebar_wrapper.style.marginTop = `${navbar.clientHeight}px`;
+// stripes sind die einzelnen Striche im Hamburger icon, also seine "Kinder (children)"
+const stripes = hamburger_icon.children;
 
 
-// onclick Eventhandler
+// (margin Top des sidebar_wrappers wird automatisch angepasst, damit keine Zwischenräume entstehen)
+// *wurde weggelassen, da nicht benötigt*
+
+// onclick Eventhandler für den Hamburger Button
 window.addEventListener('load', (e) =>{
     hamburger_icon.addEventListener('click', () => {
-        sidebar.classList.toggle("hidden");
-        console.log("Click");
+        sidebar_links.classList.toggle("hidden");
+        sidebar_wrapper.classList.toggle("blur");
+        sidebar.classList.toggle("sidebar-hidden");
+        sidebar.classList.toggle("sidebar-visible");
+    });
+
+    // wenn man mit der Maus über den Hamburger button hovert, wird mithilfe einer for-Schleife
+    // allen "stripes" die Klasse "stripe_onhover" zugefügt
+    hamburger_icon.addEventListener('mouseenter', (e) => {
+        for (let i = 0; i < stripes.length; i++){
+            stripes[i].classList.add("stripe_onhover");
+        }
+    });
+
+    // wenn man mit der Maus aus dem Hamburger button herausfährt, wird mithilfe derselben 
+    // for-Schleife bei allen "stripes" die Klasse "stripe_onhover" entfernt
+    hamburger_icon.addEventListener('mouseleave', (e) => {
+        for (let i = 0; i < stripes.length; i++){
+            stripes[i].classList.remove("stripe_onhover");
+        }
     });
 });
 
